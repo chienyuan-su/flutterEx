4d99570598fe9187a089e995625557cdb780dd3b
chienyuan-su <57435706+chienyuan-su@users.noreply.github.com>
2019-11-22 00:28:24 +0800

    add 3rd package

--- flutter_overall/lib/main.dart
+++ flutter_overall/lib/main.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/scheduler/ticker.dart';
+import 'package:loading_animations/loading_animations.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyLoading());
+}
 
-class MyApp extends StatelessWidget {
+class MyText1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Text('Hello, world!',
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center,
+        style: new TextStyle(color: Colors.black, fontSize: 24));
+  }
+}
+
+class MyText2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+        style: new TextStyle(color: Colors.black, fontSize: 24),
+      ),
+    );
+  }
+}
+
+class MyText3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -16,5 +46,17 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class MyLoading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SpinKitRotatingCircle(
+      color: Colors.black,
+      size: 50.0
+    );
+
   }
 }
